8a96c8cafc1f02bf33e3acdfbfec87c7040ba938
GabrielArsenio <gabrielarsenioo@gmail.com>
2021-03-25 20:11:32 -0300

    Ajuste PATH

--- src/services/exec.service.js
+++ src/services/exec.service.js
@@ -1,6 +1,14 @@
 const execute = require('child_process').exec
 
+const env = {
+    ...process.env
+}
+
+if (!env.PATH.includes('.asdf')) {
+    env.PATH = `${env.HOME}/.asdf/shims:${env.HOME}/.asdf/bin:${env.PATH}`
+}
+
 export const exec = command =>
     new Promise(resolve =>
-        execute(command, (error, stdout, stderr) =>
+        execute(command, { env } ,(error, stdout, stderr) =>
             resolve((stdout + stderr).replace(/  +/g, ' '))))
